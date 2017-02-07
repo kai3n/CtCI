e504b3c5a4b99c9374cea252d48fa2fafd9b0bf2
Mingun Pak <mingunpa@usc.edu>
2017-02-06 16:56:44 -0800

    02/06/17 need to look over again [try again?] Yes

---
+++ 2.2 Return Kth to Last.py
@@ -0,0 +1,12 @@
+def kth_to_last(ll, k):
+    runner = current = ll.head
+    for i in range(k):
+        if runner is None:
+            return None
+        runner = runner.next
+
+    while runner:
+        current = current.next
+        runner = runner.next
+
+    return current
